c386c41aa36712c5046eb2e0034a3490a9fef0b1
Magpie <edchavez821@gmail.com>
2021-03-16 14:41:45 -0600

    Added Axios and Home component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Home() {
+  return (
+    <div className="main-div">
+      <h1> This is the Home Page biatch </h1>
+    </div>
+  );
+}
+
+export default Home;
