79dff9a04d7f7f44ffeea92e0602266031084e78
Magpie <edchavez821@gmail.com>
2021-03-23 12:16:07 -0600

    Drag and drop: Capture item data on drag start

--- src/components/SearchItem.js
+++ src/components/SearchItem.js
@@ -4,12 +4,18 @@ import styles from "./SearchItem.module.css";
 function SearchItem(props){
     const elementImages = props.element.type === "track" ? props.element.album.images : props.element.images
     const itemCoverArt = elementImages.length ? elementImages[0].url : "https://via.placeholder.com/150"
-    const artistName = props.element.type === "artist" ? "" : <div className={styles.artistName}> {props.element.artists[0].name} </div>
+    const artistName = props.element.type === "artist" ? "" 
+        : <div className={styles.artistName}> {props.element.artists[0].name} </div>
+
+    const handleDrag = (e, data) => {
+        console.log(data)
+        e.dataTransfer.setData("data", data)
+    }
 
     return (
-        <div className={styles.itemCard}>
+        <div draggable onDragStart={(e) => handleDrag(e, props.element)} className={styles.itemCard}>
             <div className={styles.imageContainer}>
-                <img className={styles.itemImage} src={itemCoverArt}></img>
+                <img draggable="false" className={styles.itemImage} src={itemCoverArt}></img>
             </div>
             <div className={styles.name}> {props.element.name} </div>
             {artistName}
