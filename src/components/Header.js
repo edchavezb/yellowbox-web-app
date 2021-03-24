faa15d054071ebc957f42f32070e8a2969a049b2
Magpie <edchavez821@gmail.com>
2021-03-24 13:09:03 -0600

    Extracted Header component from Layout

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory } from "react-router-dom";
+
+import styles from "./Header.module.css";
+
+function Header(props) {
+
+  const [input, setInput] = useState("");
+  const history = useHistory();
+
+  useEffect(() => {
+    if (input) history.push(`/search/${input}`)
+  }, [input])
+
+  return (
+    <div id={styles.header}> 
+        <div id={styles.logoSide}>
+          <div id={styles.logoContainer}>
+            <img id={styles.logo} src="/logoybx.png"></img> YBX
+          </div>
+        </div>
+        <div id={styles.headerTools}>
+          <div id={styles.searchBox}>
+            <div id={styles.inputWrapper}>
+              <input id={styles.searchInput} type="text" onChange={(e) => setInput(e.target.value.trim().replace(" ", "%20"))} 
+                onFocus={() => {if(input) history.push(`/search/${input}`)}}/>
+            </div>
+            <img id={styles.searchIcon} src="/icons/search.svg"></img>
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default Header;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,35 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { useHistory } from "react-router-dom";
 
 import SideBar from "./SideBar"
+import Header from "./Header"
 import styles from "./Layout.module.css";
 
 function Layout(props) {
 
-  const [input, setInput] = useState("");
-  const history = useHistory();
-
-  useEffect(() => {
-    if (input) history.push(`/search/${input}`)
-  }, [input])
-
   return (
     <div id={styles.layout}>
-      <section id={styles.header}>
-        <div id={styles.logoSide}>
-          <div id={styles.logoContainer}>
-            <img id={styles.logo} src="/logoybx.png"></img> YBX
-          </div>
-        </div>
-        <div id={styles.headerTools}>
-          <div id={styles.searchBox}>
-            <div id={styles.inputWrapper}>
-              <input id={styles.searchInput} type="text" onChange={(e) => setInput(e.target.value.trim().replace(" ", "%20"))} 
-                onFocus={() => {if(input) history.push(`/search/${input}`)}}/>
-            </div>
-            <img id={styles.searchIcon} src="/icons/search.svg"></img>
-          </div>
-        </div>
-
+      <section id={styles.headerWrapper}>
+        <Header boxes={props.userBoxes} dispatch={props.dispatch}/>
       </section>
       <section id={styles.sideBar}>
         <SideBar userName="Pablo Alboran" boxes={props.userBoxes} dispatch={props.dispatch}/>
