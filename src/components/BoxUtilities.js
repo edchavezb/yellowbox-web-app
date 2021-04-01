0c0f86e24e2772c5759c12bc671a75b6029b7cdf
Magpie <edchavez821@gmail.com>
2021-03-31 18:08:50 -0600

    Extracted BoxUtilities component from BoxDetail

---
+++ src/components/BoxUtilities.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+
+import styles from "./BoxUtilities.module.css";
+
+function BoxUtilities(props) {
+
+  const box = props.box
+  const singleTypeBox = props.singleTypeBox
+  const visibility = props.visibility
+  const setVisibility = props.setVisibility
+
+  const handleSectionVisibility = e => {
+    const section = e.currentTarget.getAttribute("handles")
+    let newVisibility = {}
+    if (visibility[section]){
+      newVisibility[section] = false
+    }
+    else 
+      newVisibility[section] = true
+    setVisibility(state => ({...state, ...newVisibility}))
+  }
+
+  return (
+    <div id={styles.boxUtilities}>
+      <div id={singleTypeBox ? styles.noInclude : styles.includeToggler}>
+        <span id={styles.includeTitle}> Include </span>
+        {box.artists.length ?
+          <div className={visibility.artists ? styles.includeButton : styles.includeButtonPressed} onClick={(e) => handleSectionVisibility(e)} handles="artists">
+            <img className={styles.buttonIcon} src="/icons/artist.svg" />
+          </div>
+          : ""}
+        {box.albums.length ?
+          <div className={visibility.albums ? styles.includeButton : styles.includeButtonPressed} onClick={(e) => handleSectionVisibility(e)} handles="albums">
+            <img className={styles.buttonIcon} src="/icons/album.svg" />
+          </div>
+          : ""}
+        {box.tracks.length ?
+          <div className={visibility.tracks ? styles.includeButton : styles.includeButtonPressed} onClick={(e) => handleSectionVisibility(e)} handles="tracks">
+            <img className={styles.buttonIcon} src="/icons/song.svg" />
+          </div>
+          : ""}
+      </div>
+    </div>
+  )
+}
+
+export default BoxUtilities;
--- src/pages/BoxDetail.js
+++ src/pages/BoxDetail.js
@@ -1,35 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
+import BoxUtilities from '../components/BoxUtilities';
 import BoxSection from '../components/BoxSection';
 import styles from "./BoxDetail.module.css";
 
 function BoxDetail(props) {
 
-  console.log(props.userBoxes)
   const params = useParams()
   const box = props.userBoxes.find(box => box.id === params.id)
   const boxNotEmpty = box.albums.length > 0 || box.artists.length > 0 || box.tracks.length > 0;
   const singleTypeBox = [box.albums, box.artists, box.tracks].filter((box) => box.length > 0).length === 1
 
-  console.log(singleTypeBox)
-
   const [visibility, setVisibility] = useState({artists: true, albums: true, tracks: true})
-
-  const handleSectionVisibility = e => {
-    const section = e.currentTarget.getAttribute("handles")
-    let newVisibility = {}
-    if (visibility[section]){
-      newVisibility[section] = false
-    }
-    else 
-      newVisibility[section] = true
-    setVisibility(state => ({...state, ...newVisibility}))
-  }
-
-  const includeToggler = () => {
-    
-  }
+  const [boxSorting, setBoxSorting] = useState({artists: "custom", albums: "custom", tracks: "custom"})
 
   useEffect(() => {
     console.log(visibility)
@@ -37,28 +21,7 @@ function BoxDetail(props) {
 
   return (
     <div id={styles.mainPanel}>
-      {boxNotEmpty ? 
-        <div id={styles.boxUtilities}>
-          <div id={singleTypeBox ? styles.noInclude : styles.includeToggler}> 
-            <span id={styles.includeTitle}> Include </span>
-            {box.artists.length ? 
-              <div className={visibility.artists ? styles.includeButton : styles.includeButtonPressed} onClick={(e) => handleSectionVisibility(e)} handles="artists"> 
-                <img className={styles.buttonIcon} src="/icons/artist.svg" /> 
-              </div> 
-              : ""}
-            {box.albums.length ? 
-              <div className={visibility.albums ? styles.includeButton : styles.includeButtonPressed} onClick={(e) => handleSectionVisibility(e)} handles="albums"> 
-                <img className={styles.buttonIcon} src="/icons/album.svg" /> 
-              </div>  
-              : ""}
-            {box.tracks.length ? 
-              <div className={visibility.tracks ? styles.includeButton : styles.includeButtonPressed} onClick={(e) => handleSectionVisibility(e)} handles="tracks"> 
-                <img className={styles.buttonIcon} src="/icons/song.svg" /> 
-              </div>  
-              : ""}
-          </div>
-        </div>
-      : ""}
+      {boxNotEmpty ? <BoxUtilities box={box} singleTypeBox={singleTypeBox} visibility={visibility} setVisibility={setVisibility}/> : ""}
       <h2> {box.name} </h2>
       <p> {box.description} </p>
       {box.artists.length ? <BoxSection type="Artists" data={box.artists} visible={visibility.artists} /> : ""}
