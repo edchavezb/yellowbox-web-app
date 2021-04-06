1936415a64b11c6cf7517fd6d64b9032772c8994
Magpie <edchavez821@gmail.com>
2021-04-06 12:42:29 -0500

    Drag actions dropzones with icons

--- src/components/DragActions.js
+++ src/components/DragActions.js
@@ -3,11 +3,46 @@ import React, { useState, useEffect } from 'react';
 import styles from "./DragActions.module.css";
 
 function DragActions(props) {
+
+  const handleDragEnter = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  const handleDragLeave = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    e.currentTarget.className = `${styles.dragActionsButton} ${styles.idleColor}`
+  }
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    e.currentTarget.className = `${styles.dragActionsButton} ${styles.dragOverColor}`
+  }
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const data = JSON.parse(e.dataTransfer.getData("data"))
+  }
+
   return (
     <div id={props.elementDragging ? styles.actionsActive : styles.actionsHidden}>
-      <div className={styles.dragActionsButton} action="delete"></div>
-      <div className={styles.dragActionsButton} action="add-to-box"></div>
-      <div className={styles.dragActionsButton} action="add-to-subsection"></div>
+      <div className={`${styles.dragActionsButton} ${styles.idleColor}`} id={styles.delete} action="delete"
+        onDragEnter={(e) => handleDragEnter(e)} 
+        onDragLeave={(e) => handleDragLeave(e)}
+        onDragOver={(e) => handleDragOver(e)}
+        onDrop={(e) => handleDrop(e)}> 
+        <img id={styles.deleteImg} src="/icons/delete.svg"></img> 
+      </div>
+      <div className={`${styles.dragActionsButton} ${styles.idleColor}`} id={styles.addTo} action="add-to"
+        onDragEnter={(e) => handleDragEnter(e)} 
+        onDragLeave={(e) => handleDragLeave(e)}
+        onDragOver={(e) => handleDragOver(e)}
+        onDrop={(e) => handleDrop(e)}> 
+        <img id={styles.addToImg} src="/icons/plus.svg"></img> 
+      </div>
     </div>
   )
 }
