d59e2cc81549016a5dabf9d0c74a7c6f04c78d00
edchavezb <edgp.chavez@gmail.com>
2022-02-21 18:15:03 -0600

    Refactored GridItem with destructuring assignment

--- src/components/box-views/GridItem.js
+++ src/components/box-views/GridItem.js
@@ -1,38 +1,38 @@
-import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 
 import styles from "./GridItem.module.css";
 
-function GridItem(props) {
-	const elementImages = props.element.type === "track" ? props.element.album.images : props.element.images;
+function GridItem({element, setElementDragging}) {
+	const {name, type, album, images, artists, owner, uri, id} = element;
+	const elementImages = type === "track" ? album.images : images;
 	const itemCoverArt = elementImages.length ? elementImages[0].url : "https://via.placeholder.com/150"
-	const artistName = props.element.type === "playlist" || props.element.type === "artist" ? ""
-		: <Link to={`/detail/artist/${props.element.artists[0].id}`}><div className={styles.artistName}> {props.element.artists[0].name} </div> </Link>;
-	const ownerName = props.element.type === "playlist" ? <a href={props.element.owner.uri}><div className={styles.artistName}> {props.element.owner.display_name} </div></a> : "";
+	const artistName = type === "playlist" || type === "artist" ? ""
+		: <Link to={`/detail/artist/${artists[0].id}`}><div className={styles.artistName}> {artists[0].name} </div> </Link>;
+	const ownerName = type === "playlist" ? <a href={owner.uri}><div className={styles.artistName}> {owner.display_name} </div></a> : "";
 
 	const handleDrag = (e, data) => {
 		console.log(data)
 		e.dataTransfer.setData("data", JSON.stringify(data))
-		props.setElementDragging(true)
+		setElementDragging(true)
 	}
 
 	const handleDragEnd = () => {
-		props.setElementDragging(false)
+		setElementDragging(false)
 	}
 
 	return (
-		<div draggable onDragStart={(e) => handleDrag(e, props.element)} onDragEnd={() => handleDragEnd()} className={styles.itemCard}>
+		<div draggable onDragStart={(e) => handleDrag(e, element)} onDragEnd={() => handleDragEnd()} className={styles.itemCard}>
 			<div className={styles.imageContainer}>
-				<a href={`${props.element.uri}:play`}>
+				<a href={`${uri}:play`}>
 					<div className={styles.instantPlay}>
 						<img className={styles.spotifyIcon} src='/icons/spotify_icon.png' alt='spotify'></img>
-						{props.element.type === "track" ? <span> Play </span> : <span> Open </span>}
+						{type === "track" ? <span> Play </span> : <span> Open </span>}
 					</div>
 				</a>
-				<img draggable="false" className={styles.itemImage} alt={props.element.name} src={itemCoverArt}></img>
+				<img draggable="false" className={styles.itemImage} alt={name} src={itemCoverArt}></img>
 			</div>
-			<Link to={`/detail/${props.element.type}/${props.element.id}`}> <div className={styles.name}> {props.element.name} </div> </Link>
-			{props.element.type !== "playlist" ?
+			<Link to={`/detail/${type}/${id}`}> <div className={styles.name}> {name} </div> </Link>
+			{type !== "playlist" ?
 				artistName :
 				ownerName}
 		</div>
